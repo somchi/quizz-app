75a8617992d600e7f4a488ad8e75e0e6beeff472
jeffreyai7 <jirukeh@gmail.com>
2025-06-07 17:41:12 +0100

    create question and answer component

---
+++ src/app/components/AnswerCom.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AnswerCom = () => {
+  return <div>AnswerCom</div>;
+};
+
+export default AnswerCom;
---
+++ src/app/components/QuestionCom.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuestionCom = () => {
+  return <div>QuestionCom</div>;
+};
+
+export default QuestionCom;
